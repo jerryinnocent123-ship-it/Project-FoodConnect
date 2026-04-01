8fad2040d36ef9eb6845062ed67c8c8e16842a6e
Jerry Innocent <jerryinnocent123@gmail.com>
2026-04-01 12:57:43 -0400

    creation paj resto a

---
+++ src/pages/Restaurant.jsx
@@ -0,0 +1,27 @@
+export default function Restaurant() {
+    return (
+        <>
+
+            {/* 👉 Tout restoran yo
+
+Li dwe genyen:
+
+Liste restoran
+Filtre (prix, localisation)
+Search bar */}
+
+
+            {/*  Restaurant Details
+
+👉 Lè user klike sou yon restoran
+
+Li dwe genyen:
+
+Non restoran
+Menu (plats)
+Pri
+Bouton “Add to cart”
+🛒 4. Cart (Pan */}
+        </>
+    )
+}
