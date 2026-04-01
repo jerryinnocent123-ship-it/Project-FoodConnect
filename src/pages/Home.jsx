40b423f25fdef43c4e1fdd616629ab3312ea51da
Jerry Innocent <jerryinnocent123@gmail.com>
2026-04-01 12:43:59 -0400

    creation paje 1

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+    return (
+        <>
+ // Li dwe genyen:
+
+        // Logo + navbar
+        // Lis restoran (cards)
+        // Rechèch restoran
+        // // Bouton “Commander”
+
+        </>
+    )
+}
