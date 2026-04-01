e750070f2b9b7aa1668073d8895dcb82ee254d1f
Jerry Innocent <jerryinnocent123@gmail.com>
2026-04-01 13:00:02 -0400

    creation paj Charts l a

---
+++ src/pages/Panier.jsx
@@ -0,0 +1,13 @@
+export default function () {
+    return (
+        <>
+            {/* Kè aplikasyon an
+
+Li dwe genyen:
+
+Lis pwodwi chwazi
+Total prix
+Bouton “Commander” */}
+        </>
+    )
+}
