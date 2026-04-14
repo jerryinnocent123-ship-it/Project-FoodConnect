f76ef5368cce6bac92f0f32f3c2d38f8ff61d003
Jerry Innocent <jerryinnocent123@gmail.com>
2026-04-14 14:26:37 -0400

    navigation yo

--- src/components/client/Hero.jsx
+++ src/components/client/Hero.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+// sa se hero component ki pral montre yon imaj ak yon mesaj akeyi itilizatè a sou paj dakèy la. 
+// Li kapab gen ladan tou yon bouton pou ankouraje itilizatè a pou li eksplore plis sou sit la
+//  oswa pou li enskri.
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <>
+      <div>Hero</div>
+      <h1>Welcome to FoodConnect</h1>
+    </>
+
   )
 }
 
