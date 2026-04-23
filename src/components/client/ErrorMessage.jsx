6a2c744b915e01e1a789322618db2a5381c66e78
Convengton <conventonvilbrun@gmail.com>
2026-04-23 15:12:06 -0400

    Ajoute ContactForm component

--- src/components/client/ErrorMessage.jsx
+++
@@ -1,9 +0,0 @@
-export const ErrorMessage = ({ message }) => {
-  if (!message) return null
-  
-  return (
-    <div className="bg-red-50 border border-red-200 text-red-600 px-4 py-3 rounded-lg">
-      {message}
-    </div>
-  )
-}
