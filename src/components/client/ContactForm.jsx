c1257eef62b66a5a710b4170828a07b11fd9fe21
Jerry Innocent <jerryinnocent123@gmail.com>
2026-04-28 21:32:59 -0400

    try to resolve problem

--- src/components/client/ContactForm.jsx
+++ src/components/client/ContactForm.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { useTranslation } from 'react-i18next'
 
 export default function ContactForm() {
+    const { t } = useTranslation()
   const [formData, setFormData] = useState({ mail: "", username: "", message: "" });
 
   const handleChange = (event) => {
