354d94ec42c81990a6bd33124e5375f676d187d7
Lumenes Lyse <lysalumenes17@gmail.com>
2026-05-13 23:05:24 -0400

    bg hero

--- src/components/client/Hero.jsx
+++ src/components/client/Hero.jsx
@@ -1,10 +1,14 @@
-// sa se hero component ki pral montre yon imaj ak yon mesaj akeyi itilizatè a sou paj dakèy la. 
-// Li kapab gen ladan tou yon bouton pou ankouraje itilizatè a pou li eksplore plis sou sit la
-//  oswa pou li enskri.
+
 
 function Hero() {
   return (
     <>
+     
+
+
+
+
+
       <div>Hero</div>
       <h1>Welcome to FoodConnect</h1>
     </>
